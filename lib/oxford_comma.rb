9992b9490dad0a34116aa1033269d639d8cec643
Karen G <kgraves0701@gmail.com>
2018-03-15 18:12:09 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,9 @@ def oxford_comma(array)
   #case x
     if array.count == 1
     "#{[array].join(",")}"
-  else #array.count > 1
-    array.each do |element|
+  elsif array.count == 2
+    array[0] + " and " + array[1]
+  # else #array.count > 1
+  #   array.each do |element|
   end
 end
