816c25eaa81bf9aefdb5e67a9f63342676f8e5f5
Karen G <kgraves0701@gmail.com>
2018-03-15 17:54:52 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,8 @@
 def oxford_comma(array)
   string = ""
-  array.each do |element|
-    string += element
-  end
-  string
+  case 
+  when array.count == 1
+    string = array[0]
+  when array.count == 2
+    string = array[0] + " and " + array[1]
 end
