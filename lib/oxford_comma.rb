4094e8a743fba76bc69e1ed92e5040079f76b9fd
Karen G <kgraves0701@gmail.com>
2018-03-15 18:04:35 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,22 +1,4 @@
 def oxford_comma(array)
-  string = ""
-  case
-  when array.count == 1
-    string = array[0]
-  when array.count == 2
-    string = array[0] + " and " + array[1]
-  when array.count == 3
-    string = array[0] + ", " + array[1] + "," + " and " + array[2]
-  when array.count > 3
-    counter = 0
-    string = array[0]
-    while counter < array.count
-      array.each do |element|
-      string = string + ", " + element
-    end
-    string
-    # string = string + " and " + array[counter]
-    # string
-  end
+  "'#{[array].join("','")}'"
 
 end
