e2266a31ade55055ea8db120b3200eb067215ec2
Karen G <kgraves0701@gmail.com>
2018-03-15 18:23:36 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,7 @@ def oxford_comma(array)
   if array.count == 1
     string << array[0]
   elsif array.count == 2
-    array[0] + " and " + array[1]
+    string << array[0] + " and " + array[1]
   else
     holder = array.pop
     string = "#{[array].join(", ")}"
