33d28e80181b005c67f7ef1dc3d398883bd70c38
Karen G <kgraves0701@gmail.com>
2018-03-15 18:13:54 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,7 +6,7 @@ def oxford_comma(array)
   elsif array.count == 2
     array[0] + " and " + array[1]
   else 
-    "#{[array].join(",")}"
+    "#{[array].join(", ")}"
   # else #array.count > 1
   #   array.each do |element|
   end
