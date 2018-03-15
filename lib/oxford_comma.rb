140a6b36f0351fdb814e68afeb77d6fbd7fe9d74
Karen G <kgraves0701@gmail.com>
2018-03-15 18:20:50 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -7,10 +7,10 @@ def oxford_comma(array)
     array[0] + " and " + array[1]
   else
     # array
-    # holder = array.pop
-    # array.pop
+    holder = array.pop
+    array.pop
     string = "#{[array].join(", ")}"
-    #string = string + " and " + array.last
+    string = string + " and " + holder
    end
   # else #array.count > 1
   #   array.each do |element|
