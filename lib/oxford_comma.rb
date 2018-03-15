0d27755bfa6cd746445bbb09e31c614e5eb96dcc
Karen G <kgraves0701@gmail.com>
2018-03-15 17:56:38 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,11 +1,11 @@
 def oxford_comma(array)
   string = ""
-  case 
+  case
   when array.count == 1
     string = array[0]
   when array.count == 2
     string = array[0] + " and " + array[1]
   when array.count == 3
-    string = array[0] + "," + array[1] + "," + " and " + array[2]
+    string = array[0] + ", " + array[1] + "," + " and " + array[2]
   end
 end
