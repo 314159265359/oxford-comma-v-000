d4a5a0d1a552ce6ef398abad7fecd4997e6e7a9f
Karen G <kgraves0701@gmail.com>
2018-03-15 18:21:48 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,13 +6,11 @@ def oxford_comma(array)
   elsif array.count == 2
     array[0] + " and " + array[1]
   else
-    # array
     holder = array.pop
-    #array.pop
     string = "#{[array].join(", ")}"
     string = string + ", and " + holder
    end
   # else #array.count > 1
   #   array.each do |element|
-  
+
 end
