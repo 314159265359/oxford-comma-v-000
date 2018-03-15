5993abaf57859fde4ba43c1aa9c256ac44fafd66
Karen G <kgraves0701@gmail.com>
2018-03-15 17:59:58 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -7,5 +7,14 @@ def oxford_comma(array)
     string = array[0] + " and " + array[1]
   when array.count == 3
     string = array[0] + ", " + array[1] + "," + " and " + array[2]
+  when array.count > 3
+    counter = 0
+    string = array[0]
+    while counter < array.count
+      array.each do |element|
+      string = string + ", " + element
+    end
+    string = string + " and " + array[counter]
   end
+
 end
